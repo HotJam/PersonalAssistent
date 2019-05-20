0ad6ad6392337c2985b0ccb719b703f8c981594f
Nuno Campos <hotjam@live.com.pt>
2019-05-20 15:50:11 +0100

    Update-Database

--- PersonalAssistent/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ PersonalAssistent/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PersonalAssistent.Models;
 
 namespace PersonalAssistent.Data.Migrations
 {
--- PersonalAssistent/Startup.cs
+++ PersonalAssistent/Startup.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using PersonalAssistent.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PersonalAssistent.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace PersonalAssistent
 {
@@ -43,11 +35,11 @@ namespace PersonalAssistent
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            var connection = @"Server=DESKTOP-TFQ3JM9;Database=aspnet-PersonalAssistent-0BC4D3A2-B5AD-4C58-8044-CA2C4FE46ED6;Trusted_Connection=True;MultipleActiveResultSets=true";
-            services.AddDbContext<Models.AssistentePessoalContext>(options => options.UseSqlServer(connection));
+            var connection = @"Server=DESKTOP-TFQ3JM9;Database=AssistentePessoal;Trusted_Connection=True;MultipleActiveResultSets=true";
+            services.AddDbContext<AssistentePessoalContext>(options => options.UseSqlServer(connection));
 
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<Models.AssistentePessoalContext>();
+            //services.AddDefaultIdentity<IdentityUser>()
+            //    .AddEntityFrameworkStores<Models.AssistentePessoalContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
